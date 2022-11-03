58baf8c24b59db2ed7d3e9283b9778c77a09d63c
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-03 12:50:28 +0400

    update ex.2

--- Chapter 2- Variables & Comments/exercise 2.py
+++ Chapter 2- Variables & Comments/exercise 2.py
@@ -1,2 +1,1 @@
-print('Albert Einstein once said, "A person who never made a mistake')
-print('never tried anything new."')
+print('Albert Einstein once said, “A person who never made a mistake never tried anything new.')
