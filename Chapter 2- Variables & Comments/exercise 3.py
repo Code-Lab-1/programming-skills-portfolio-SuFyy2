fdc59bca6300d4e300de0eeae89f2f2c04d1fbe4
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-03 12:20:24 +0400

    new code ex 2.3

---
+++ Chapter 2- Variables & Comments/exercise 3.py
@@ -0,0 +1,13 @@
+name = "tEric Matthes"
+
+print("Unmodified:")
+print(name)
+
+print("Using lstrip():")
+print(name.lstrip())
+
+print("Using rstrip():")
+print(name.rstrip())
+
+print("Using strip():")
+print(name.strip())
