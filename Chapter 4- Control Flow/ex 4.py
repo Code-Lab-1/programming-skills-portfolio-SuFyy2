3ca98aa7158d13915bae10fe576a5fdbc10c18eb
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-05 15:49:43 +0400

    new code 4.4

---
+++ Chapter 4- Control Flow/ex 4.py
@@ -0,0 +1,13 @@
+age=0.6
+if age==65 or age>65:
+  print("person is an elder")
+elif age==20 or age>20:
+  print("person is an adult")
+elif age==13 or age>13:
+  print("person is a teenager")
+elif age==4 or age>4:
+  print("person is a kid")
+elif age==2 or age>2:
+  print("person is a toddler")
+else:
+  print("person is a baby")
