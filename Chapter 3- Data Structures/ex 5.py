05a682ddd312a8d0626644a9444185bcb62ca602
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-05 14:48:18 +0400

    new code ch-3,ex3,ex4,ex5

---
+++ Chapter 3- Data Structures/ex 5.py
@@ -0,0 +1,11 @@
+names=["Shezin","Jamnas","Rizwan"]
+print("Hello!,I am keeping a dinner at my house,would you like to come",names[0],"?")
+print("\nYo",names[1],"I am planning a dinner night at my place,Make sure are free at that day")
+print("\nHello",names[2],"Can you come this weekend for dinner at my place ?")
+print("""\nCant come for the dinner!
+\t""",names[2])
+del names[2]
+names.insert(2,"salman")
+print("\nHello!,I am keeping a dinner at my house,would you like to come",names[0],"?")
+print("\nYo",names[1],"I am planning a dinner night at my place,Make sure are free at that day")
+print("\nHello",names[2],"Can you come this weekend for dinner at my place ?")
