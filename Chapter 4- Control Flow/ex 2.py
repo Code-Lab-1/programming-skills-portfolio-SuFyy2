be2247bcd95a5cd8462bb8ab080973abeb25c451
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-05 15:14:16 +0400

    new code ex 4.2

---
+++ Chapter 4- Control Flow/ex 2.py
@@ -0,0 +1,12 @@
+#if green
+alien_color="green"
+if alien_color=="green":
+  print("player just earned 5 points")
+else:
+  print("player just earned 10 points")
+#else green
+alien_color="red"
+if alien_color=="green":
+  print("player just earned 5 points")
+else:
+  print("player just earned 10 points")
