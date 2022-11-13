2bbfa259902b8529f814ea155a4d644b72fed95e
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-13 17:20:57 +0400

    new code's added in ch-5

---
+++ Chapter 5- Dictionaries/ex 10.py
@@ -0,0 +1,5 @@
+student = {"name":"ahmed","age":"18","class":"year1","gender":"male","location":"uae","email":"abc@gmail.com","mobile":"46578363"}
+student["name"] ="ali"
+student.update({"class":"year2","location":"dubai","mobile":"1233455"})
+student.pop("nationality")
+student.popitem()
