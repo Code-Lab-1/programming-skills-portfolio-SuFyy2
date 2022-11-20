99c19e46a7591f54a880eb7dd6ee67ca15360ed4
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-20 11:54:17 +0400

    new code ch-3

---
+++ Chapter 3- Data Structures/ex 6.py
@@ -0,0 +1,9 @@
+#3.6
+my_list = [1, 2, 3]
+print(my_list)
+my_list.append([555, 12])
+print(my_list)
+my_list.extend([234, 'more_example'])
+print(my_list)
+my_list.insert(1, 'insert_example')
+print(my_list)
