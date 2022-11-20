2930704c5581a33708820620a34896dfd832a587
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-20 11:58:06 +0400

    update codes

---
+++ Chapter 4- Control Flow/ex 6.py
@@ -0,0 +1,12 @@
+#BMI calculator
+weight=int(input())
+height=float(input())
+BMI= weight / height**2
+if BMI <18.5:
+  print("Underweight")
+elif BMI <25:
+  print("Normal")
+elif BMI <30:
+  print("Overweight")
+else:
+  print("Obesity")
