414c19a1cbb5d77115ac7eecaec4d7d074155a66
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-20 12:41:12 +0400

    new update with new code for ch1

--- Chapter 2- Variables & Comments/exercise 7.py
+++ Chapter 2- Variables & Comments/exercise 7.py
@@ -1,3 +1,8 @@
 #2.7
-a='Hello \nWorld! \nWelcome'
-print(a)
+x = 5
+y = 10
+temp = x
+x = y
+y = temp
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
