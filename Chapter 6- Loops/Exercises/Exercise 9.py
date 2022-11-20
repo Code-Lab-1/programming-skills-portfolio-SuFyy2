badf8151fd42fb705b51a93ececb19984d6d5862
SuFyy2 <muhammedsufiyanck22@gmail.com>
2022-11-20 15:59:37 +0400

    update

--- Chapter 6- Loops/Exercises/Exercise 9.py
+++ Chapter 6- Loops/Exercises/Exercise 9.py
@@ -1,11 +1,8 @@
-#Find the total price of the ticket for 5 people of any age
-prompt="Under 3yr ticket is free\nEnter your age "
-total=0
-i=0
-while i<5:
-  age= int(input(prompt))
-  i = i+1
-  if age<3:
-    continue
-  total +=100
-print(total)
+#6.9
+i = 3
+
+while i>=0:
+
+   print(i)
+
+   i = i - 1
